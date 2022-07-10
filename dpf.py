aa24d7fdc3b919811a6c10d35ec04f27ae0954b0
AlexanderUp <alexander.uperenk0@yandex.ru>
2022-07-11 00:57:35 +0300

    Code refactored

--- dpf.py
+++ dpf.py
@@ -1,67 +1,69 @@
-# encoding:utf-8
-# find file duplicates in given folder
+'''Find file duplicates in given folder.'''
 
-
-import os
 import argparse
-import hashlib
+import fnmatch
+import os
 import shutil
 import sys
-import fnmatch
-import datetime
+from datetime import datetime
+from typing import Generator
 
-from sqlalchemy import create_engine
 from sqlalchemy import func
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import mapper
 
 import dpf_model as dbm
+from dpf_aux import get_hash, get_session
+from dpf_model import FileHash
 
-from dpf_model import HashTable
+mapper(dbm.FileHash, dbm.table_hashes)
 
-mapper(dbm.HashTable, dbm.table_hashes)
-
-BLOCK_SIZE = 1024 * 1024 # one megabyte
-MEDIA_EXTENSIONS = ['avi', 'mp3', 'mp4', 'mkv', 'webm', 'mpg', 'jpg', 'png']
-DOCS_EXTENSIONS = ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'numbers', 'pages', 'djvu', 'txt', 'epub', 'png', 'jpg', 'gif', 'zip', 'rar', 'djv']
+BLOCK_SIZE = 1024 * 1024  # one megabyte
+MEDIA_EXTENSIONS = ['.avi', '.mp3', '.mp4', '.mkv', '.webm', '.mpg', '.jpg',
+                    '.png']
+DOCS_EXTENSIONS = ['.pdf', '.doc', '.docx', '.xls', '.xlsx', '.numbers',
+                   '.pages', '.djvu', '.djv', '.txt', '.epub', '.png',
+                   '.jpg', '.gif', '.zip', '.rar',
+                   ]
 TRASHBIN = os.path.expanduser('~/.Trash')
-
 DB_NAME = 'hash_db.sqlite3'
-DB_BACKUP_NAME_PATTERN = 'hash_db.sqlite3_backup_{}_{}_{}_{}_{}'
+DB_BACKUP_NAME_PATTERN = '{}_backup_{}_{}_{}_{}_{}'
 BACKUP_MAX_NUMBER = 5
 
 
 class Duplifinder():
+    '''
+    Looking for file duplicates in specified folder taking into account
+    calculated hash.
+    '''
 
-    def __init__(self, path, path_to_be_excluded):
+    def __init__(self, path: str, path_to_be_excluded: str) -> None:
         '''
         Current limitations: only one excluded folders supported.
         '''
         if os.path.isdir(path):
             self._path = path
         else:
-            raise TypeError('Target path is not specified or is not a directory!')
+            raise TypeError('Target path is not specified or is not'
+                            'a directory!')
 
-        if path_to_be_excluded == None or os.path.isdir(path_to_be_excluded):
+        if path_to_be_excluded is None or os.path.isdir(path_to_be_excluded):
             self._path_to_be_excluded = path_to_be_excluded
         else:
-            raise TypeError('Excluded path is not specified or is not a directory!')
+            raise TypeError('Excluded path is not specified or is not'
+                            'a directory!')
 
-        self._path_to_db = os.path.join(self._path, DB_NAME)
+        self._path_to_db: str = os.path.join(self._path, DB_NAME)
+        self.session = get_session(self._path_to_db)
 
-        self.engine = create_engine('sqlite:///' + self._path + os.sep + DB_NAME)
-        dbm.metadata.create_all(bind=self.engine)
-        Session = sessionmaker(bind=self.engine)
-        self.session = Session()
-        self._extensions = None
-        return None
-
-    def _create_db_backup(self):
+    def _create_db_backup(self) -> None:
         if os.path.exists(self._path_to_db):
             print('Database already exists! Backuping...')
-            time = datetime.datetime.now()
-            db_backup_name = DB_BACKUP_NAME_PATTERN.format(time.year, time.month, time.day, time.hour, time.minute)
-            path_to_db_backup = os.path.join(self._path, db_backup_name)
+            time: datetime = datetime.now()
+            db_backup_name: str = DB_BACKUP_NAME_PATTERN.format(
+                DB_NAME, time.year, time.month, time.day, time.hour,
+                time.minute)
+            path_to_db_backup: str = os.path.join(self._path, db_backup_name)
 
             try:
                 shutil.copyfile(self._path_to_db, path_to_db_backup)
@@ -72,69 +74,80 @@ class Duplifinder():
             else:
                 print('Backup created!')
 
-        backup_files = fnmatch.filter(os.listdir(self._path), 'hash_db.sqlite3_backup_*')
-        backup_files = [(backup_file, os.stat(os.path.join(self._path, backup_file)).st_birthtime) for backup_file in backup_files]
-        backup_files.sort(key=lambda file: file[1])
+        backup_file_names: list = fnmatch.filter(os.listdir(self._path),
+                                                 f'{DB_NAME}_backup_*')
+        backup_file_names.sort(
+            key=lambda file: os.stat(
+                os.path.join(self._path, file)
+                ).st_birthtime
+            )
 
-        for file in backup_files[:-BACKUP_MAX_NUMBER]:
-            shutil.move(os.path.join(self._path, file[0]), os.path.join(TRASHBIN, file[0]))
-        print('Backup cleared!')
-        return None
+        for file in backup_file_names[:-BACKUP_MAX_NUMBER]:
+            try:
+                shutil.move(os.path.join(self._path, file),
+                            os.path.join(TRASHBIN, file))
+            except OSError as err:
+                print(err)
+                print('Error during deleting old db backups! Exiting...')
+        print('Backups cleared!')
 
     def _clean_up_db(self):
-        print('Cleaning database..')
-        files = self.session.query(HashTable).all()
+        print('Cleaning database...')
+        files: list = self.session.query(FileHash).all()
         for file in files:
             if not os.path.exists(file.path):
-                print(f'File doesn\'t exist! Deleting from db... {file.path}')
-                self.session.delete(file)
+                print('File doesn\'t exist! Marking as has been deleted...\t'
+                      f'{file.path}')
+                file.is_deleted = True
         try:
             self.session.commit()
         except Exception as err:
             print(err)
             self.session.rollback()
-        print('Database clean!')
-        return None
+            print('DB cleaning failed.')
+        else:
+            print('Database clean!')
 
-    def set_extensions(self, extensions):
-        self._extensions = extensions
+    def set_extensions(self, extensions: list[str]) -> None:
+        self._extensions: list[str] = extensions
 
-    @staticmethod
-    def get_hash(file, block_size = BLOCK_SIZE):
-        with open(file, 'br') as f:
-            hasher = hashlib.sha256()
-            while True:
-                binary_content = f.read(block_size)
-                if binary_content:
-                    hasher.update(binary_content)
-                else:
-                    break
-        return hasher.hexdigest()
-
-    def update_db(self):
+    def update_db(self) -> None:
         if self._extensions:
             self._create_db_backup()
             for root, dirs, files in os.walk(self._path):
                 for file in files:
                     path = os.path.join(root, file)
-                    if self._path_to_be_excluded and os.path.commonpath((self._path_to_be_excluded, path)) == self._path_to_be_excluded:
-                        print(f'>>>> Excluded: <{path}>')
+
+                    if self._path_to_be_excluded:
+                        if os.path.commonpath(
+                                (self._path_to_be_excluded,
+                                 path)) == self._path_to_be_excluded:
+                            print(f'>>>> Excluded: <{path}>')
+                            continue
+
+                    _, extention = os.path.splitext(file)
+                    extention.lower()
+                    if extention not in self._extensions:
+                        print('==== Passed! File type is not allowed!'
+                              f'<{file}>')
                         continue
 
-                    if file.split('.')[-1].lower() not in self._extensions:
-                        print(f'==== Passed! File type is not allowed! <{file}>')
-                        continue
-
-                    is_path_to_file_in_db = self.session.query(HashTable).filter(HashTable.path==path).first()
+                    is_path_to_file_in_db: FileHash = (
+                        self.session
+                        .query(FileHash)
+                        .filter(FileHash.path == path)
+                        .first()
+                        )
                     if is_path_to_file_in_db:
                         print(f'>>>> Already in db! <{path}>')
                         continue
 
-                    hash = self.get_hash(path)
-                    creation_time = os.stat(path).st_birthtime
+                    hash: str = get_hash(path, block_size=BLOCK_SIZE)
+                    creation_time: float = os.stat(path).st_birthtime
                     print(f'******** Adding: <{path}> **** Added!')
+
+                    self.session.add(FileHash(hash, path, creation_time))
                     try:
-                        self.session.add(HashTable(hash, path, creation_time))
                         self.session.commit()
                     except Exception as err:
                         print(err)
@@ -143,57 +156,103 @@ class Duplifinder():
         else:
             print('No extensions specified!')
         self.session.close()
-        return None
 
-    def feed_duplicate_hash_query(self):
-        '''Returns list of duplicate enrties (instances of class <HashTable>), not a list of hashes itself.'''
-        duplicates = self.session.query(HashTable).group_by(HashTable.hash).having(func.count(HashTable.hash)>1).all()
-        print(f'Duplicated hashes found: {len(duplicates)}')
+    def duplicate_hash_query_generator(self) -> Generator[list, None, None]:
+        '''
+        Yields list of duplicate enrties (instances of class <FileHash>),
+        not a list of hashes itself.
+        '''
+        duplicate_hash_entities: list[tuple[str]] = (
+            self.session
+                .query(FileHash.hash)
+                .group_by(FileHash.hash)
+                .having(func.count(FileHash.hash) > 1)
+                .all()
+            )
+        print(f'Duplicated hashes found: {len(duplicate_hash_entities)}')
+        duplicate_hashes = (entity[0] for entity in duplicate_hash_entities)
         print('************************************')
-        for duplicate in duplicates:
-            query = self.session.query(HashTable).filter(HashTable.hash==duplicate.hash).all()
+        for duplicate_hash in duplicate_hashes:
+            query: list[FileHash] = (
+                self.session
+                    .query(FileHash)
+                    .filter(FileHash.hash == duplicate_hash)
+                    .all()
+                )
             query.sort(key=lambda duplicate: duplicate.creation_time)
             yield query
 
-    def print_duplicates_list(self):
-        for query in self.feed_duplicate_hash_query():
+    def print_duplicates_list(self) -> None:
+        for query in self.duplicate_hash_query_generator():
             for duplicate in query:
                 print(f'{duplicate.hash} =>> {duplicate.path}')
             print('************************************')
         self.session.close()
-        return None
 
     def remove_duplicates(self):
-        for query in self.feed_duplicate_hash_query():
+        for query in self.duplicate_hash_query_generator():
             for duplicate in query[1:]:
-                self.session.delete(duplicate)
+                duplicate.is_deleted = True
                 try:
-                    shutil.move(duplicate.path, TRASHBIN + os.sep + os.path.basename(duplicate.path))
+                    shutil.move(duplicate.path,
+                                os.path.join(TRASHBIN,
+                                             os.path.basename(duplicate.path)))
                 except OSError as err:
                     print(err)
                 else:
                     print(f'Deleteted! {duplicate.path}')
         try:
             self.session.commit()
-        except sqlalchemy.exc.SQLAlchemyError as err:
+        except SQLAlchemyError as err:
             print(err)
             self.session.rollback()
-        self.session.close()
-        return None
+        else:
+            print('Files deleted, db updated.')
+        finally:
+            self.session.close()
 
 
 if __name__ == '__main__':
     print('=' * 75)
 
-    parser = argparse.ArgumentParser(description='Find file duplicates in given directory')
+    parser = argparse.ArgumentParser(
+        description='Find file duplicates in given directory'
+        )
     parser.add_argument('path_to_be_processed')
-    parser.add_argument('-e', '--exclude', default=None, help='Folders to be excluded from processing', dest='path_to_be_excluded')
+    parser.add_argument(
+        '-e',
+        '--exclude',
+        default=None,
+        help='Folders to be excluded from processing',
+        dest='path_to_be_excluded'
+        )
     type_group = parser.add_mutually_exclusive_group()
-    type_group.add_argument('-m', '--media', action='store_true', help='Update database with hashes of media files in given directory')
-    type_group.add_argument('-d', '--documents', action='store_true', help='Update database with hashes of document and non-media files in given directory')
+    type_group.add_argument(
+        '-m',
+        '--media',
+        action='store_true',
+        help='Update database with hashes of media files in given directory'
+        )
+    type_group.add_argument(
+        '-d',
+        '--documents',
+        action='store_true',
+        help=('Update database with hashes of document and'
+              'non-media files in given directory')
+        )
     action_group = parser.add_mutually_exclusive_group()
-    action_group.add_argument('-r', '--remove_duplicates', action='store_true', help='Find and delete file duplicates')
-    action_group.add_argument('-l', '--list', action='store_true', help='Print list of file duplicates')
+    action_group.add_argument(
+        '-r',
+        '--remove_duplicates',
+        action='store_true',
+        help='Find and delete file duplicates'
+        )
+    action_group.add_argument(
+        '-l',
+        '--list',
+        action='store_true',
+        help='Print list of file duplicates'
+        )
     args = parser.parse_args()
 
     print('Running Duplifinder...')
--- dpf_model.py
+++ dpf_model.py
@@ -1,34 +1,37 @@
-# encoding:utf-8
-# database model for duplifinder
-
-
-from sqlalchemy import MetaData
-from sqlalchemy import Table
-from sqlalchemy import Column
-from sqlalchemy import Integer
-from sqlalchemy import String
-
+'''Database model for duplifinder.'''
 
 import os
 
+from sqlalchemy import Boolean, Column, Integer, MetaData, String, Table
 
 metadata = MetaData()
 
 
 table_hashes = Table('hashes', metadata,
-                    Column('id', Integer, primary_key=True),
-                    Column('hash', String, nullable=False),
-                    Column('path', String, unique=True, nullable=False),
-                    Column('creation_time', Integer, nullable=False),
-                    )
+                     Column('id', Integer, primary_key=True),
+                     Column('hash', String, nullable=False),
+                     Column('path', String, unique=True, nullable=False),
+                     Column('creation_time', Integer, nullable=False),
+                     Column('is_deleted', Boolean, default=False,
+                            nullable=False),
+                     )
 
 
-class HashTable():
+class FileHash():
+    '''Represents FileHash entry.'''
 
-    def __init__(self, hash, path, creation_time):
+    def __init__(self,
+                 hash: str,
+                 path: str,
+                 creation_time: float,
+                 ) -> None:
         self.hash = hash
         self.path = path
         self.creation_time = creation_time
+        self.is_deleted = False
 
-    def __repr__(self):
-        return f'<Hash: {self.hash}, creation time: {self.creation_time}, path: {os.path.basename(self.path)}>'
+    def __repr__(self) -> str:
+        path: str = os.path.basename(self.path)
+        return (f'<Hash: {self.hash}'
+                f', creation time: {self.creation_time}'
+                f', path: {path}>')
