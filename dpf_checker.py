f66a2d50b9ea6b9c655ff6c9c64eb9043c4e7fc5
AlexanderUp <alexander.uperenk0@yandex.ru>
2023-01-07 20:32:14 +0300

    Approx hashing speed calculation added

--- dpf_checker.py
+++ dpf_checker.py
@@ -1,17 +1,11 @@
-'''Duplicate checker.'''
-
 import os
 import shutil
 import sys
 from time import perf_counter
 
-from sqlalchemy.orm import mapper
-
 from config import Config
 from dpf_aux import get_hash, get_session
-from dpf_model import FileHash, table_hashes
-
-mapper(FileHash, table_hashes)
+from dpf_model import FileHash
 
 config = Config()
 
@@ -26,6 +20,11 @@ class DpfChecker():
         self.folder = folder
         self.extensions = extensions
         self.session = get_session(self._path_to_db)
+        self._size_processed = 0
+
+    def get_human_readable_size(self):
+        '''Convert size of processed files in Mb.'''
+        return self._size_processed / 1024 / 1024
 
     def check(self) -> None:
         for root, dirs, files in os.walk(self.folder):
@@ -34,15 +33,16 @@ class DpfChecker():
                 if extension not in self.extensions:
                     continue
 
-                path_to_file: str = os.path.join(
-                    root, file
-                )
+                path_to_file: str = os.path.join(root, file)
                 hash: str = get_hash(path_to_file,
                                      block_size=config.BLOCK_SIZE)
                 is_exists: tuple = (self.session
                                         .query(FileHash.hash)
                                         .filter_by(hash=hash)
                                         .one_or_none())
+
+                self._size_processed += os.path.getsize(path_to_file)
+
                 if is_exists:
                     print(f'>>> To be deleted: {path_to_file}', end='\t')
                     dest_trashbin_path: str = os.path.join(config.TRASHBIN,
@@ -68,9 +68,15 @@ if __name__ == '__main__':
     print(f'Folder: {folder}')
 
     checker = DpfChecker(path_to_db, folder, config.MEDIA_EXTENSIONS)
+
     start = perf_counter()
     checker.check()
     elapsed = perf_counter() - start
+    gigabytes_processed = checker.get_human_readable_size()
+
     print(f'Time elapsed: {elapsed}')
+    print('Total processed:', gigabytes_processed, 'Mb')
+    print('Avarage hashing speed:', gigabytes_processed / elapsed, 'Mb/second (hashlib.sha256)')
+    print('>> NB! Calculated speed affected by db quering overhead! <<')
 
     print('-' * 125)
